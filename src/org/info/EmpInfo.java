e0ead56297058710d52828a496221e5b8b4d76ae
karthikook <karthikapreethi03@gmail.com>
2021-12-26 13:44:27 +0530

    its done

---
+++ src/org/info/EmpInfo.java
@@ -0,0 +1,20 @@
+package org.info;
+
+
+public class EmpInfo {
+	private void name() {
+		System.out.println("Karthi");
+	}
+
+	private void phoNo() {
+	System.out.println("7397569479");
+	}
+	public static void main(String[] args) {
+		EmpInfo e=new EmpInfo();
+		e.name();
+		e.phoNo();
+	}
+	}
+
+
+
